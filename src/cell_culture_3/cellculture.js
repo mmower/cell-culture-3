af5bd29699c9199f404f8856a8246da155b44a70
Matt Mower <self@mattmower.com>
2013-05-04 14:05:35 +0100

    Factored the 'Game of Life' rules out of the cellculture mechanics.

--- src/cell_culture_3/cellculture.js
+++ src/cell_culture_3/cellculture.js
@@ -1,127 +1,58 @@
 (function( cellculture, $, undefined ) {
-  var zero_func = function() { return 0; };
 
-  cellculture.colourings = [
-    "#FFFFFF",
-    "#800000"
-  ];
+  cellculture.setup = function( paper, dim, options ) {
+    options = options || {};
 
-  cellculture.live_p = 0.273;
-
-  cellculture.rules = [
-    [0,0,0,1,0,0,0,0,0],
-    [0,0,1,1,0,0,0,0,0]
-  ];
-
-  cellculture.setup = function( paper, dim, stable_func ) {
-    var history = [new_matrix(dim,zero_func),new_matrix(dim,zero_func),new_matrix(dim,zero_func)];
-    var matrix = new_matrix( dim, function() { return Math.random() < cellculture.live_p ? 1 : 0; } );
+    var matrix = cellculture.initial_matrix( dim, options );
     var shapes = create_shapes( paper, matrix );
     update_shapes( matrix, shapes );
 
     return function() {
-      var h;
-      matrix = evolve_matrix( matrix );
-      if( h = recurrent_matrix( matrix, history ) ) {
-        stable_func(h);
-      }
-      history = [history[1],history[2],matrix];
+      matrix = evolve_matrix( matrix, options );
       update_shapes( matrix, shapes );
-    }
+    };
   };
 
 
-  function recurrent_matrix( matrix, history ) {
-    for( var i in history ) {
-      if( matrix_is_same( matrix, history[i] ) ) {
-        return i;
-      }
-    }
-    return false;
-  }
-
-
-  function matrix_is_same( a, b ) {
-    if( a.length != b.length ) {
-      return false;
-    }
-    for( var i = 0; i < a.length; i++ ) {
-      if( a[i] != b[i] ) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-
-  function new_matrix( dim, cell_func ) {
-    var matrix = Array( dim * dim );
-    matrix.dim = dim;
-    matrix.t = 0;
-    for( var i = 0; i < dim*dim; i++ ) {
-      matrix[i] = cell_func();
-    }
-    return matrix;
-  }
-
-
-  function create_shapes( paper, matrix ) {
-    var coords,
-        shapes = Array( matrix.length );
-    for( var i = 0; i < matrix.length; i++ ) {
-      rect = cell_rect( cell_position(matrix,i), matrix.dim, paper.width, paper.height );
-      shapes[i] = paper.rect(
-        rect.x,
-        rect.y,
-        rect.w,
-        rect.h,
-        1
-      );
-    }
-    return shapes;
-  }
-
-
-  function update_shapes( matrix, shapes ) {
-    var i;
-    for( i = 0; i < matrix.length; i++ ) {
-      shapes[i].attr( "fill", cell_colour( matrix[i] ) );
-    }
+  cellculture.new_matrix = function( dim, cell_func ) {
+    return {
+      dim: dim,
+      gen: 0,
+      cells: cell_generator( dim, cell_func )
+    };
   }
 
 
   function evolve_matrix( matrix ) {
-    var succ = new_matrix( matrix.dim, zero_func ),
-        neighbours;
-    succ.t = matrix.t + 1;
-    for( var i = 0; i < matrix.length; i ++ ) {
-      neighbours = living_neighbours(matrix,i);
-//      console.log( i+":"+neighbours );
-      succ[i] = cellculture.rules[matrix[i]][neighbours];
-    }
-    return succ;
+    return {
+      dim: matrix.dim,
+      gen: matrix.gen + 1,
+      cells: cell_generator( matrix.dim, function(i) { return cellculture.gen_fun(matrix,i) } )
+    };
   }
 
 
-  function living_neighbours( matrix, i ) {
-    var pos = cell_position( matrix, i ),
-        scan_pos,
-        living_neighbours = 0;
-
-    for( var y = -1; y < 2; ++y ) {
-      for( var x = -1; x < 2; ++x ) {
-        if( x != 0 || y != 0 ) {
-//          console.log( "x="+x+",y="+y );
-          living_neighbours += matrix[cell_address( matrix, offset_cell_position( matrix, pos, x, y ) )];
-        }
-      }
-    }
-
-    return living_neighbours;
+  function cell_generator( dim, cell_func ) {
+    return _.range( dim * dim ).map( function(i) { return cell_func( i ) } );
   }
 
 
-  function cell_position( matrix, i ) {
+  function create_shapes( paper, matrix ) {
+    return _.range( matrix.dim * matrix.dim ).map( function( i ) {
+      var rect = cell_rect( cellculture.cell_position(matrix,i), matrix.dim, paper.width, paper.height );
+      return paper.rect( rect.x, rect.y, rect.w, rect.h, 1 );
+    });
+  }
+
+
+  function update_shapes( matrix, shapes ) {
+    _.each( shapes, function(shape,i) {
+      shape.attr( "fill", cellculture.cell_colour( matrix, i ))
+    });
+  }
+
+
+  cellculture.cell_position = function( matrix, i ) {
     return {
       y: Math.floor( i / matrix.dim ),
       x: i % matrix.dim
@@ -129,26 +60,27 @@
   }
 
 
-  function axis_shift( matrix, v, delta ) {
-    v += delta;
-    if( v < 0 ) {
-      v += matrix.dim;
-    } else if( v >= matrix.dim ) {
-      v -= matrix.dim;
+  function offset_index_wrapped( matrix, v, delta ) {
+    var fv = v + delta;
+    if( fv < 0 ) {
+      return fv + matrix.dim;
+    } else if( fv >= matrix.dim ) {
+      return fv - matrix.dim;
+    } else {
+      return fv;
     }
-    return v;
   }
 
 
-  function offset_cell_position( matrix, pos, dx, dy ) {
+  cellculture.offset_cell_position = function( matrix, pos, dx, dy ) {
     return {
-      x: axis_shift( matrix, pos.x, dx ),
-      y: axis_shift( matrix, pos.y, dy )
+      x: offset_index_wrapped( matrix, pos.x, dx ),
+      y: offset_index_wrapped( matrix, pos.y, dy )
     };
   }
 
 
-  function cell_address( matrix, pos ) {
+  cellculture.cell_address = function( matrix, pos ) {
     return ( pos.y * matrix.dim ) + pos.x;
   }
 
@@ -160,11 +92,6 @@
       w: width / dim,
       h: height / dim
     };
-  }
-
-
-  function cell_colour( life ) {
-    return cellculture.colourings[Math.min(life,cellculture.colourings.length-1)];
   }
 
 }( window.cellculture = window.cellculture || {}, jQuery ));
---
+++ src/cell_culture_3/gameoflife.js
@@ -0,0 +1,76 @@
+(function( cellculture, $, undefined ){
+  var colourings = [
+    "#FFFFFF",
+    "#800000"
+  ];
+
+
+  var rules = [
+    [0,0,0,1,0,0,0,0,0],
+    [0,0,1,1,0,0,0,0,0]
+  ];
+
+
+  var live_p = 0.273;
+
+
+  cellculture.initial_matrix = function( dim, options ) {
+    return cellculture.new_matrix( dim, living_cell );
+  }
+
+
+  cellculture.gen_fun = function( matrix, i ) {
+    return rules[matrix.cells[i]][living_neighbours(matrix,i)];
+  }
+
+
+  cellculture.cell_colour = function( matrix, i ) {
+    return colourings[Math.min(matrix.cells[i],colourings.length-1)];
+  }
+
+
+  function living_cell() {
+    return Math.random() < live_p ? 1 : 0;
+  }
+
+
+  /*
+  function recurrent_matrix( matrix, history ) {
+    for( var i in history ) {
+      if( matrix_is_same( matrix, history[i] ) ) {
+        return i;
+      }
+    }
+    return false;
+  }
+
+
+  function matrix_is_same( a, b ) {
+    if( a.length != b.length ) {
+      return false;
+    }
+    for( var i = 0; i < a.length; i++ ) {
+      if( a[i] != b[i] ) {
+        return false;
+      }
+    }
+    return true;
+  }
+  */
+
+  function living_neighbours( matrix, i ) {
+    var pos = cellculture.cell_position( matrix, i ),
+        living_neighbours = 0;
+
+    for( var y = -1; y < 2; ++y ) {
+      for( var x = -1; x < 2; ++x ) {
+        if( x != 0 || y != 0 ) {
+          living_neighbours += matrix.cells[cellculture.cell_address( matrix, cellculture.offset_cell_position( matrix, pos, x, y ) )];
+        }
+      }
+    }
+
+    return living_neighbours;
+  }
+
+}(window.cellculture = window.cellculture || {}, jQuery))
